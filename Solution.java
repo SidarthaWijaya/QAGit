c1a93aaaf91dbe2f1404ff4ef07d78075d305527
Sidartha Wijaya <66309126+SidarthaWijaya@users.noreply.github.com>
2024-05-14 13:36:49 +0700

    tambah file baru

---
+++ Solution.java
@@ -0,0 +1,39 @@
+// import java.util.Arrays;
+// //soal lat 14
+// public class test2 {
+//     public static boolean isAnagram(String str1, String str2) {
+//         // Convert both strings to lowercase
+//         str1 = str1.toLowerCase();
+//         str2 = str2.toLowerCase();
+
+//         // Check if both strings have the same length
+//         if (str1.length()!= str2.length()) {
+//             return false;
+//         }
+
+//         // Create a char array for each string
+//         char[] arr1 = str1.toCharArray();
+//         char[] arr2 = str2.toCharArray();
+
+//         // Sort both arrays
+//         Arrays.sort(arr1);
+//         Arrays.sort(arr2);
+
+//         // Compare the sorted arrays
+//         return Arrays.equals(arr1, arr2);
+//     }
+
+//     public static void main(String[] args) {
+//         String str1 = "CAT";
+//         String str2 = "tac";
+
+//         if (isAnagram(str1, str2)) {
+//             System.out.println("The strings are anagrams");
+//         } else {
+//             System.out.println("The strings are not anagrams");
+//         }
+//     }
+// }
+
+
+
